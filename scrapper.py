52b22de6e44686377013c0564c44eae547e6bcc7
Toma Andrei-Paul <toma1.andrei47@gmail.com>
2021-12-13 17:23:10 +0200

    added explanations + small adjustments

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,8 @@
+from genericpath import getctime
 import urllib.request
 from bs4 import BeautifulSoup
 import os
+from time import time
 
 # URL to States of the world Wikipedia page
 states_of_the_world = "https://en.wikipedia.org/wiki/List_of_sovereign_states"
@@ -12,44 +14,47 @@ states_of_the_world_links = []
 
 def get_and_write_URLs():
     """
-    getURLs function obtains the URLs of each state located on Wikipedia
+
+    get_and_write_URLs() function obtains the URLs of each state located on
+    Wikipedia 'List of states' page with BeautifulSoup library and saves content
+    on states_of_the_world.txt file. File is renewed only if there are more than
+    3 days past from file creation.
+
     """
 
-    if os.path.exists("states_of_the_world.txt"):
-        pass
+    if (
+        not os.path.exists("states_of_the_world.txt")
+        or time() - os.path.getctime("states_of_the_world.txt") >= 259200
+    ):
+        # obtaining 'List of states' page content
+        req = urllib.request.urlopen(states_of_the_world)
+        all_states_page = req.read().decode("utf-8")
 
-    # obtaining all states page content
-    all_states_page = urllib.request.urlopen(states_of_the_world)
+        # create a BeautifulSoup instance with html parser for easy manipulation of page content
+        soup = BeautifulSoup(all_states_page, "html.parser")
 
-    # create a BeautifulSoup instance with html parser for easy manipulation of page content
-    soup = BeautifulSoup(all_states_page, "html.parser")
+        # select all anchor tags from states table
+        anchorTags = soup.select("table > tbody > tr > td > b > a")
 
-    anchorTags = soup.select("table > tbody > tr > td > b > a")
+        # get the url of each state and save it to a list
+        for a in anchorTags:
+            states_of_the_world_links.append(a["href"])
 
-    for a in anchorTags:
-        states_of_the_world_links.append(a["href"])
+        # remove old file if it exists
+        if os.path.exists("states_of_the_world.txt"):
+            os.remove("states_of_the_world.txt")
 
-    file = open("states_of_the_world.txt", "a")
+        # open and write to file full URL of each state
+        file = open("states_of_the_world.txt", "a", encoding="utf-8")
 
-    for url in states_of_the_world_links:
-        file.write(base_path + url + "\n")
+        for url in states_of_the_world_links:
+            file.write(base_path + url + "\n")
 
-    file.close()
-
-
-def obtain_infos():
-    for link in states_of_the_world_links:
-        url = base_path + link
-        print(link)
-
-
-def test_stuff():
-    print(os.path.getctime("scrapper.py"))
+        file.close()
 
 
 def main():
-    # get_and_write_URLs()
-    test_stuff()
+    get_and_write_URLs()
 
 
 if __name__ == "__main__":
